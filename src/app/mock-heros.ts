83f278dbb015502b3662b22a85db70584eede495
Ozoezi Chukwujekwu Stanley <43678258+DrStanley@users.noreply.github.com>
2021-11-15 16:42:10 +0100

    Hero App Initial

---
+++ src/app/mock-heros.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice' ,rank:1},
+  { id: 12, name: 'Narco' ,rank:4},
+  { id: 13, name: 'Bombasto',rank:6 },
+  { id: 14, name: 'Celeritas',rank:10 },
+  { id: 15, name: 'Magneta' ,rank:9},
+  { id: 16, name: 'RubberMan',rank:2 },
+  { id: 17, name: 'Dynama' ,rank:3},
+  { id: 18, name: 'Dr IQ' ,rank:7},
+  { id: 19, name: 'Magma',rank:8 },
+  { id: 20, name: 'Tornado' ,rank:5}
+];
